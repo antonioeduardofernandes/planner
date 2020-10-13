f0dfeffffb7c213f8631ce4c79a7b8b6b6b1522b
antonio <antonioeduardofernandes@gmail.com>
2020-10-13 17:57:05 -0300

    default styles added to project

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,43 @@
 <template>
-  
+  <div id="app"></div>
 </template>
 
 <style>
+:root {
+  --color-accent: #6a6fe9;
+  --color-background: #ffffff;
+  --color-background-secondary: #eef1f2;
+  --color-text: #484d56;
+  --color-warning: #e9c46a;
+  --color-danger: #e9726a;
+  --color-placeholder: #484d56;
+  --color-text-white: #e5e5e5;
+}
 
+::selection {
+  display: none;
+}
+
+*,
+html,
+body {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+
+body {
+  width: 100vw;
+  height: 100%;
+  font-size: 12px;
+  overflow: hidden;
+}
+
+#app {
+  height: 100vh;
+  width: 100%;
+  background-color: var(--color-background);
+  color: var(--color-text);
+}
 </style>
